a7878bd3fa053d5de2c8f4163368bd00386b44ea
akateason <xietianchen@subaojaojiang.com>
2015-12-09 12:42:32 +0800

    Initial Commit

---
+++ XTNetReloader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTNetReloader
+//
+//  Created by TuTu on 15/12/9.
+//  Copyright © 2015年 teason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XTNetReloader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XTNetReloader
+//
+//  Created by TuTu on 15/12/9.
+//  Copyright © 2015年 teason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
